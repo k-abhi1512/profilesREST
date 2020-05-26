d359e7d799cc45ef9b29d216b831fab9d2963540
Avhii <2akj1018@gmail.com>
2020-05-27 00:18:11 +0530

    Added Profile Feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
+
 # Create your models here.
 
 class UserProfileManager(BaseUserManager):
@@ -11,7 +13,7 @@ class UserProfileManager(BaseUserManager):
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
-            raise ValuError("User must have an email address")
+            raise ValueError("User must have an email address")
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
@@ -54,3 +56,17 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """return string representation of our user"""
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -12,19 +12,29 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.UserProfile
         fields = ('id', 'email', 'name', 'password')
-        axtra_kwargs = {
+        extra_kwargs = {
             'password': {
                 'write_only': True,
                 'style': {'input_type': 'password'}
             }
         }
 
-def create(self, validated_data):
-    """Create and return a new user"""
-    user = models.UserProfile.objects.create_user(
-        email = validated_data['email'],
-        name = validated_data['name'],
-        password = validated_data['password']
-    )
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email = validated_data['email'],
+            name = validated_data['name'],
+            password = validated_data['password']
+        )
 
-    return user
+        return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed item"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {
+            'user_profile': {'read_only': True}
+        }
